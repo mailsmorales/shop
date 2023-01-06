b93e008ef7b18e3797aaaec8f90975390305a891
mukhamed-Ali <mailsmorales21@mail.ru>
2023-01-06 14:46:51 +0300

    add img, npm i mui

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import SearchIcon from '@mui/icons-material/Search';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import { Link } from 'react-router-dom';
 
 export const Navbar = () => {
   return (
-    <div className=''></div>
+    <div className='navbar'>
+      <div className="wrapper">
+      <div className="left">
+        <div className="item">
+          <img src="./img/en.png" alt="" />
+          <KeyboardArrowDownIcon />
+        </div>
+        <div className="item">
+          <span>USD</span>
+          <KeyboardArrowDownIcon />
+        </div>
+        <div className="item">
+          <Link to='/products/1'>Man</Link>
+        </div>
+      </div>
+      <div className="center"></div>
+      <div className="right"></div>
+      </div>
+    </div>
   )
 }
