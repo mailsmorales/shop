db9139c921a02bfb8d51d9b7ec5b7baaa8c3bc09
mukhamed-Ali <mailsmorales21@mail.ru>
2023-01-11 02:57:33 +0300

    fixed card design

--- src/components/FeturedProducts/FeaturedProducts.jsx
+++ src/components/FeturedProducts/FeaturedProducts.jsx
@@ -7,7 +7,7 @@ export const FeaturedProducts = ({ type }) => {
     {
       id: 1,
       img: "https://img.alicdn.com/imgextra/i3/92688455/O1CN01zUWToT2CKROJ2Obe5_!!0-item_pic.jpg",
-      img2: "https://img.alicdn.com/imgextra/i2/92688455/O1CN01EsTVXP2CKROYiE0TG_!!0-item_pic.jpg",
+      img2: "https://img5.lalafo.com/i/posters/original/ea/7f/5e/3759b03d6d7fad8b7cbe100aa1.jpeg",
       title: "Long Sleeve Graphic T-shirt",
       isNew: true,
       oldPrice: 19,
@@ -15,7 +15,8 @@ export const FeaturedProducts = ({ type }) => {
     },
     {
       id: 2,
-      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIRyuGHeoCNCKykdmbImNwMwJm_9GPRr59fajOTYVIzuDS9rOJkawGlMiqQO9j0boMSZ0&usqp=CAU",
+      img: "https://img.alicdn.com/imgextra/i3/3586957598/O1CN01ZEJAlv25zvsgEZI0P_!!3586957598.jpg",
+      img2: "https://img.alicdn.com/imgextra/i3/3586957598/O1CN01Au9QhL25zvseovcaz_!!0-item_pic.jpg",
       title: "coat",
       isNew: true,
       oldPrice: 19,
