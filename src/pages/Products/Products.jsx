68cd416ecdc5a219d5fa54fd2a81c3681f01b7f8
mukhamed-Ali <mailsmorales21@mail.ru>
2023-01-15 23:49:31 +0300

    fixed style app

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -13,20 +13,20 @@ const [sort, setSort] = useState(null)
     <div className="products">
       <div className="left">
         <div className="filterItem">
-          <h2>Product Categories</h2>
-          <div className="inputItem">
-            <input type="checkbox" id="1" value={1} />
-            <label htmlFor="1">Shoes</label>
-          </div>
-          <div className="inputItem">
-            <input type="checkbox" id="2" value={2} />
-            <label htmlFor="2">Skirts</label>
-          </div>
-          <div className="inputItem">
-            <input type="checkbox" id="3" value={3} />
-            <label htmlFor="3">Coats</label>
-          </div>
+        <h2>Product Categories</h2>
+        <div className="inputItem">
+          <input type="checkbox" id="1" value={1} />
+          <label htmlFor="1">Shoes</label>
         </div>
+        <div className="inputItem">
+          <input type="checkbox" id="2" value={2} />
+          <label htmlFor="2">Skirts</label>
+        </div>
+        <div className="inputItem">
+          <input type="checkbox" id="3" value={3} />
+          <label htmlFor="3">Coats</label>
+        </div>
+      </div>
         <div className="filterItem">
           <h2>Filter by price</h2>
           <div className="inputItem">
